bfa94dddfe1bc3b7ecd1222bc5bc363b8040a6c6
Jon Winn <me@jonwinn.com>
2013-10-12 13:20:55 -0700

    moved install.sh and fixed locations

--- install.sh
+++ install.sh
@@ -1,9 +1,16 @@
 #!/bin/sh
 
-endpath=$HOME/.vim
+endpath=$HOME/.dotfiles
 today=`date +%Y%m%d`
 
-if [[ -d $endpath || -f $Home/.vimrc ]]; then
+echo "Cloning from github into ${endpath}"
+if [ -d $endpath/.git ]; then
+	cd ${endpath} && git pull
+else
+	git clone https://github.com/jwinn/dotfiles.git $endpath
+fi
+
+if [[ -d $HOME/.vim || -f $HOME/.vimrc ]]; then
 	echo "Vim settings already exist"
 	read -p "Do you want to back them up? [Y/n]" yn
 	case $yn in
@@ -20,18 +27,16 @@ if [[ -d $endpath || -f $Home/.vimrc ]]; then
 	esac
 fi
 
-echo "Cloning from github into ${endpath}"
-if [ -d $endpath/.git ]; then
-	cd ${endpath} && git pull
-else
-	git clone https://github.com/jwinn/dotvim.git $endpath
-fi
-
 echo "Creating symlinks"
-ln -sf ${endpath}/vimrc ~/.vimrc
+ln -sf ${endpath}/bash_profile ~/.bash_profile
+ln -sf ${endpath}/editorconfig ~/.editorconfig
+ln -sf ${endpath}/sleep ~/.sleep
+ln -sf ${endpath}/wakeup ~/.wakeup
+ln -sf ${endpath}/vim ~/.vim
+ln -sf ${endpath}/vim/vimrc ~/.vimrc
 
-echo "Creating backup and undo folders"
-mkdir -p ${endpath}/.backup/undo
+echo "Creating vim backup and undo folders"
+mkdir -p ${endpath}/vim/.backup/undo
 
 echo "Launching Vim to install Bundles.."
 SHELL=/bin/sh vim $endpath/vundles.vim +BundleInstall! +BundleClean +qall
