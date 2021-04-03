83edf3fe4bd412c3a3b1dfd97a08e942cf54eb03
Jon Winn <me@jonwinn.com>
2021-04-02 17:45:12 -0700

    change predicate check

--- linux/install.sh
+++ linux/install.sh
@@ -1,4 +1,4 @@
-if [ -z "$(command -v is_sourced || true)" ] || ! is_sourced; then
+if [ -z "$(command -v is_sourced || true)" ] && ! is_sourced; then
   printf "File should not be called directly\n"
   exit 1
 fi
