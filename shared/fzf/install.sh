cdeb1ca3eba70c79033785cab3c1824de39af91d
Jon Winn <me@jonwinn.com>
2021-04-04 14:12:22 -0700

    update to fzf installer to not update shell scripts

--- shared/fzf/install.sh
+++ shared/fzf/install.sh
@@ -5,5 +5,11 @@ if [ -d "${FZF_DIR}" ] || [ -n "$(command -v fzf || true)" ]; then
   printf "Fzf already installed [%s]\n" "${FZF_DIR}"
 elif q_prompt "Do you want to install fzf" "y"; then
   git clone --depth 1 https://github.com/junegunn/fzf.git "${FZF_DIR}"
-  "${FZF_DIR}/install" --all --xdg
+  "${FZF_DIR}/install" --all \
+    --xdg \
+    --no-completion \
+    --no-update-rc \
+    --no-bash \
+    --no-zsh \
+    --no-fish
 fi
